ab5c3d54e8dfd3ca3654091d245e39618446750e
Andrej Kislovskij <101807823+andrejatcastai@users.noreply.github.com>
2025-12-15 13:17:11 +0200

    Use array when setting VK extra arguments for EKS (#12)

--- examples/onboarding-with-existing-eks-cluster/providers.tf
+++ examples/onboarding-with-existing-eks-cluster/providers.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 3.1.1"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.35.0"
+    }
     null = {
       source  = "hashicorp/null"
       version = ">= 3.2.4"
@@ -50,6 +54,23 @@ provider "helm" {
         var.eks_cluster_region
       ]
     }
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.eks.name,
+      "--region",
+      var.eks_cluster_region
+    ]
   }
 }
 
