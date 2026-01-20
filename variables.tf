cd453b3703690aaea57e333d50250481839d3830
anjmao <andzej.maciusovic@gmail.com>
2026-01-20 18:53:03 +0200

    Bump omni agent default version

--- variables.tf
+++ variables.tf
@@ -58,7 +58,7 @@ variable "reserved_subnet_cidrs" {
 variable "omni_agent_chart_version" {
   description = "OMNI agent helm chart version"
   type        = string
-  default     = "1.1.11"
+  default     = "1.1.12"
 }
 
 variable "skip_helm" {
