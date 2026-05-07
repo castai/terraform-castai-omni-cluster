02c3a8d4e4375604521dc46dfd3d42374fb4281a
Andrej Kislovskij <andrej@cast.ai>
2026-05-07 19:52:26 +0300

    Add VPC endpoint for ECR on EKS VPC side.

--- examples/hf-test-setup/ecr.tf
+++ examples/hf-test-setup/ecr.tf
@@ -1,3 +1,41 @@
+resource "aws_security_group" "eks_ecr_endpoints" {
+  name_prefix = "${var.cluster_name}-eks-ecr-endpoints"
+  vpc_id      = module.eks_vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [module.eks_vpc.vpc_cidr_block]
+  }
+}
+
+resource "aws_vpc_endpoint" "eks_ecr_api" {
+  vpc_id              = module.eks_vpc.vpc_id
+  service_name        = "com.amazonaws.${var.cluster_region}.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.eks_vpc.private_subnets
+  security_group_ids  = [aws_security_group.eks_ecr_endpoints.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "eks_ecr_dkr" {
+  vpc_id              = module.eks_vpc.vpc_id
+  service_name        = "com.amazonaws.${var.cluster_region}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.eks_vpc.private_subnets
+  security_group_ids  = [aws_security_group.eks_ecr_endpoints.id]
+  private_dns_enabled = true
+}
+
+# S3 gateway endpoint for ECR image layer pulls
+resource "aws_vpc_endpoint" "eks_s3" {
+  vpc_id            = module.eks_vpc.vpc_id
+  service_name      = "com.amazonaws.${var.cluster_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = concat(module.eks_vpc.private_route_table_ids, module.eks_vpc.public_route_table_ids)
+}
+
 resource "aws_ecr_repository" "hf_backbone" {
   name                 = "${var.cluster_name}-hf-backbone"
   image_tag_mutability = "MUTABLE"
