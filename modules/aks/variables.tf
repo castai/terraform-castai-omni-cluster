614b3de33e06040d20c3659196eadd1faa3fc74b
Francesco Torta <62566275+fra98@users.noreply.github.com>
2026-05-11 14:21:49 +0100

    feat: expose IPAM settings

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -23,3 +23,21 @@ variable "reserved_subnet_cidrs" {
   type        = list(string)
   default     = []
 }
+
+variable "ipam_pools" {
+  description = "Override Liqo IPAM network pools. If not set, defaults to private address space (RFC 1918)"
+  type        = list(string)
+  default     = null
+}
+
+variable "ipam_external_cidr" {
+  description = "Override Liqo IPAM externalCIDR. If not set, it will be allocated automatically by Liqo"
+  type        = string
+  default     = null
+}
+
+variable "ipam_internal_cidr" {
+  description = "Override Liqo IPAM internalCIDR. If not set, it will be allocated automatically by Liqo"
+  type        = string
+  default     = null
+}
