1c9ff05b6e524ed718590fc1320925be2aaec981
Francesco Torta <62566275+fra98@users.noreply.github.com>
2026-05-11 14:21:49 +0100

    feat: expose IPAM settings

--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -22,3 +22,21 @@ variable "reserved_subnet_cidrs" {
   description = "List of subnet CIDRs for IPAM reserved subnets"
   type        = list(string)
 }
+
+variable "ipam_pools" {
+  description = "Override Liqo IPAM network pools. If not set, defaults to private address space (RFC 1918)"
+  type        = list(string)
+  default     = null
+}
+
+variable "ipam_external_cidr" {
+  description = "Override Liqo IPAM externalCIDR. If not set, it will be allocated automatically by Liqo"
+  type        = string
+  default     = null
+}
+
+variable "ipam_internal_cidr" {
+  description = "Override Liqo IPAM internalCIDR. If not set, it will be allocated automatically by Liqo"
+  type        = string
+  default     = null
+}
