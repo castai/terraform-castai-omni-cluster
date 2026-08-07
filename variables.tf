9ccf4cd4b5b33c7788ecab80e331bdf7e0f0813c
Claudio Lorina <claudio@cast.ai>
2026-08-07 11:23:53 +0200

    chore: bump omni agent to v1.17.1

--- variables.tf
+++ variables.tf
@@ -89,7 +89,7 @@ variable "ipam_internal_cidr" {
 variable "omni_agent_chart_version" {
   description = "OMNI agent helm chart version"
   type        = string
-  default     = "1.16.3"
+  default     = "1.17.1"
 }
 
 variable "storage_provider" {
