e8202818457fea0e9dac06fbfc0967c14e478777
Francesco Torta <62566275+fra98@users.noreply.github.com>
2026-09-07 15:29:57 +0200

    chore: bump omni agent to 1.19.0 (#70)
    * bump to omni-agent 1.19.0
    
    * fixup! bump to omni-agent 1.19.0

--- variables.tf
+++ variables.tf
@@ -89,7 +89,7 @@ variable "ipam_internal_cidr" {
 variable "omni_agent_chart_version" {
   description = "OMNI agent helm chart version"
   type        = string
-  default     = "1.18.4"
+  default     = "1.19.0"
 }
 
 variable "storage_provider" {
